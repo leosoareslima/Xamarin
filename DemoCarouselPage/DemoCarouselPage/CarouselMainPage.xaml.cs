81c84f1bde83258c51c6c8d53794848ac84e0a9f
leosoareslima <leosoareslima@outlook.com>
2015-07-01 21:47:48 -0300

    Commit solution

---
+++ DemoCarouselPage/DemoCarouselPage/CarouselMainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DemoCarouselPage
+{
+    public partial class CarouselMainPage : CarouselPage
+    {
+        public CarouselMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
